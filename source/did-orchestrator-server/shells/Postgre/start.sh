72a3851a900a18a81a654fdc6e9f15b2aed6ee58
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-05-26 19:08:04 +0900

    chore: linux compatibility

--- source/did-orchestrator-server/shells/Postgre/start.sh
+++ source/did-orchestrator-server/shells/Postgre/start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 POSTGRES_PORT=$1
 POSTGRES_USER=$2
 POSTGRES_PASSWORD=$3
@@ -9,3 +11,12 @@ export POSTGRES_PASSWORD
 export POSTGRES_DB
 
 docker-compose up -d
+
+sleep 10
+
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE tas;"
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE cas;"
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE issuer;"
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE verifier;"
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE wallet;"
+docker exec -it postgre-opendid psql -U $POSTGRES_USER -c "CREATE DATABASE lss;"
